10d155c01250e665a58591a6bad946ce98267d08
MrBekzod0899 <bekzodqurbonov1399@gmail.com>
2022-02-24 09:47:29 +0500

    Header and Footer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./pages/Header";
+import Footer from "./pages/Footer"
+import Main from "./pages/Main";
+
+function App() {
+  return (
+    <div className="App">
+       <Header/>
+        <Main/>
+       <Footer/>
+     </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+         <footer class="page-footer">
+          <div class="footer-copyright">
+            <div class="container">
+              {new Date().getFullYear()} Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+  )
+}
